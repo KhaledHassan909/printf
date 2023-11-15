8891a6886078fadf819ceee82dd50a1edbd4bda6
KhaledHassan909 <Khaled.kh.hasssan@gmail.com>
2023-11-15 15:20:36 +0200

    print.c file modified

--- _print.c
+++ _print.c
@@ -1,4 +1,42 @@
 #include "main.h"
+
+/**
+ * handle_format - Function that handles different format specifiers
+ * @ptr: pointer to the current character in the format string
+ * @args: list of arguments
+ * @len: pointer to the length of the printed string
+ */
+void handle_format(const char *ptr, va_list args, unsigned int *len)
+{
+	int str_len = 0;
+
+	if (*ptr == 'c')
+	{
+		handle_char(args);
+		(*len)++;
+	}
+	else if (*ptr == 's')
+	{
+		while (ptr[str_len] != '\0')
+		{
+			str_len++;
+		}
+		handle_str(args);
+		*len += str_len;
+	}
+	else if (*ptr == '%')
+	{
+		handle_percent();
+		(*len)++;
+	}
+	else if (*ptr == 'd' || *ptr == 'i')
+	{
+		int value = va_arg(args, int);
+
+		handle_int(value);
+	}
+}
+
 /**
  * _printf - Function that produce output according to a format
  * @format: parameter
@@ -13,8 +51,10 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	if(format == NULL)
-	    return(-1);
+	if (format == NULL)
+	{
+		return (-1);
+	}
 	while (*ptr)
 	{
 		if (*ptr != '%')
@@ -27,27 +67,10 @@ int _printf(const char *format, ...)
 			ptr++;
 
 			if (*ptr == '\0')
-			        break;
-			if (*ptr == 'c')
 			{
-				handle_char(args);
-				len++;
+				break;
 			}
-			else if (*ptr == 's')
-			{
-				handle_str(args);
-			}
-			else if (*ptr == '%')
-			{
-				handle_percent();
-				len++;
-			}
-			else if (*ptr == 'd' || *ptr == 'i')
-			{
-				int value = va_arg(args, int);
-
-				handle_int(value);
-			}
+			handle_format(ptr, args, &len);
 		}
 		ptr++;
 	}
